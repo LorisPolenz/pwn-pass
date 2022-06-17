694f23078e1b06ed6d69e5b5ac90c5c36418e01d
Loris Polenz <contact@lorispolenz.com>
2022-06-17 21:15:13 +0200

    add backend code

---
+++ backend/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"backend/modules/database"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+)
+
+func getPasswordSuggestion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://pwnme:password123@localhost:27017"))
+
+	client.Connect(database.GetMongodbContext())
+
+	collection := client.Database("pwn-pass").Collection("passwords")
+
+	findOptions := options.Find()
+	findOptions.SetLimit(25)
+
+	cursor, _ := collection.Find(database.GetMongodbContext(), bson.M{"password": bson.M{"$regex": "^" + r.URL.Query().Get("q")}}, findOptions)
+
+	var results []bson.M
+
+	if err = cursor.All(database.GetMongodbContext(), &results); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(results)
+}
+
+func handleRequests() {
+	muxRouter := mux.NewRouter().StrictSlash(true)
+
+	muxRouter.Use(mux.CORSMethodMiddleware(muxRouter))
+	muxRouter.HandleFunc("/api", getPasswordSuggestion).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", muxRouter))
+}
+
+func main() {
+	handleRequests()
+}
---
+++ backend/modules/database/DatabaseImplementation.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func GetMongodbContext() context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), 100*time.Second)
+
+	return ctx
+}
+
+func GetMongodbClient() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	client.Connect(GetMongodbContext())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
